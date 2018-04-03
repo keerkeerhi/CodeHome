b26e393c2f18b47eb61294aac4636647bd742d77
mvlyforever <123278392@qq.com>
2018-04-03 09:25:29 +0800

    demo1

--- smartgraph/src/components/Demo1.vue
+++ smartgraph/src/components/Demo1.vue
@@ -1,24 +1,29 @@
 <template>
     <section>
       <svg width="300px" height="300px" xmlns="http://www.w3.org/2000/svg">
-        <line x1="0" y1="0" x2="0" y2="110" style="stroke: gray;"/>
-        <line x1="0" y1="0" x2="100" y2="0" style="stroke: gray;"/>
-        <line x1="30" y1="0" x2="30" y2="110" style="stroke: red;"/>
-        <line x1="0" y1="20" x2="100" y2="20" style="stroke: red;"/>
-
+        <g transform="translate(10,110) scale(1,-1)" >
+          <line x1="0" y1="0" x2="0" y2="110" style="stroke: gray;"/>
+          <line x1="0" y1="0" x2="100" y2="0" style="stroke: gray;"/>
+          <line x1="30" y1="0" x2="30" y2="110" style="stroke: red;"/>
+          <line x1="0" y1="20" x2="100" y2="20" style="stroke: red;"/>
+          <rect x="10" y="10" width="30" height="60" fill-opacity="0.2"
+                style="fill: black;"/>
+        </g>
         <!--<rect x="0" y="0" height="30" width="60"-->
               <!--transform="translate(30, 20) scale(3)"-->
               <!--style="fill: gray;"/>-->
 
-        <rect x="0" y="0" width="30" height="60" fill-opacity="0.2"
-              transform="translate(10,10) scale(0,-1)"
-              style="fill: black;"/>
+
       </svg>
     </section>
 </template>
 
 <script>
     export default {
-        name: "demo1"
+        name: "demo1",
+      created(){
+          console.log('===============>>>',SVGAnimatedLength)
+          console.log(SVGLength.SVG_LENGTHTYPE_EMS)
+      }
     }
 </script>
