dba03ade5e42367b131014875d52e6432ee3f68e
mvlyforever <123278392@qq.com>
2018-04-08 20:42:55 +0800

    update rote

--- smartgraph/src/components/Home.vue
+++ smartgraph/src/components/Home.vue
@@ -3,18 +3,22 @@
       <svg xmlns="http://www.w3.org/2000/svg"
            width="600" height="600" >
         <g id="mysvg">
-          <circle cx="300" cy="300" r="300" stroke="black" fill="#FF6600" ></circle>
-          <line class="line" v-for="i in number"  x1="300" y1="300" x2="0" y2="300" stroke="black" ></line>
-          <animateTransform attributeName="transform" attributeType="XML"
-                            values="0 300 300;360 300 300"
-                            keySplines="1 0 1 0"
-                            calcMode="spline" keyTimes="0 ; 1"
-                            type="rotate" begin="0s" dur="3s"
-                            fill="freeze"/>
-          <animateTransform attributeName="transform" attributeType="XML"
-                            type="rotate" begin="3s" dur="15s"
-                            to="2480 300 300"
-                            fill="freeze"/>
+          <circle cx="300" cy="300"
+                  r="300" stroke="black" fill="#FF6600" ></circle>
+          <line class="line" v-for="i in number"
+                x1="300" y1="300" x2="0" y2="300"
+                stroke="black" ></line>
+          <!--<animateTransform id="ab" attributeName="transform" attributeType="XML"-->
+                            <!--values="0 300 300;360 300 300"-->
+                            <!--keySplines="1 0 1 0"-->
+                            <!--calcMode="spline"-->
+                            <!--type="rotate" begin="0s" dur="3s"-->
+                            <!--fill="freeze" />-->
+          <!--<animateTransform attributeName="transform" attributeType="XML"-->
+                            <!--type="rotate" begin="ab.end" dur="15s"-->
+                            <!--from="360 300 300"-->
+                            <!--to="2400 300 300"-->
+                            <!--fill="freeze" />-->
         </g>
       </svg>
     </div>
@@ -36,6 +40,23 @@
           svg.selectAll('line').data(ar).attr("transform",(d)=>{
               return "rotate("+d*(360/this.number)+",300,300)"
           })
+          // svg.transition()
+          //   .duration(3000)
+          //   .attrTween("transform", ()=>{
+          //     return function(t) {
+          //       let rt = 360*t*t
+          //       return "rotate("+ rt +", 300, 300)";
+          //     }
+          //   })
+            svg.transition().delay(3000).duration(30000)
+            .attrTween("transform", ()=>{
+              return function(t) {
+                console.log(t)
+                let rt = 240*30*t
+                // console.log(rt)
+                return "rotate("+ rt +", 300, 300)";
+              }
+            });
 //          console.log(svg.transition().duration(3000).attr("width",1000))
 //          svg.transition().delay(1000).duration(3000).attr("transform","rotate(" + 359 + ",300,300)")
         }
