8fb729de3b8996355cbc38d371792aa342586513
mvlyforever <123278392@qq.com>
2018-07-05 00:06:33 +0800

    add first demo about  3d

--- d3-api-demo/demo/js/d2.js
+++ d3-api-demo/demo/js/d2.js
@@ -1,6 +1,21 @@
+let VSHARDER_SOURCE =
+    `void main(){
+            gl_Position = vec4(0.0,0.0,0.0,1.0);
+            gl_PointSize = 10.0;
+        }
+        `
+let FSHARDER_SOURCE =
+    `void main(){
+            gl_FragColor = vec4(1.0,1.0,0.0,1.0);
+        }`
+
 function main() {
     let cv = document.getElementById('mycanv');
     let ctx = getWebGLContext(cv)
-    initShaders()
+    initShaders(ctx,VSHARDER_SOURCE,FSHARDER_SOURCE)
+    ctx.clearColor(0.0,0.0,0.0,1.0)
+    ctx.clear(ctx.COLOR_BUFFER_BIT)
+
+    ctx.drawArrays(ctx.POINTS,0,1)
 }
 main()
